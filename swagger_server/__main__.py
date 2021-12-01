422e71c5e0a066b8983c2d926d5e93ca43c9cf59
Ivan Sarno <ivansarno@outlook.com>
2021-12-01 13:21:51 +0100

    add default admin

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -4,6 +4,27 @@ import connexion
 
 from swagger_server import encoder
 from swagger_server.database import db
+from swagger_server.database import User as DBUser
+from sqlalchemy.exc import IntegrityError
+
+
+def _create_admin(app):
+    with app.app.app_context():
+        try:
+            example = DBUser()
+            example.firstname = 'Admin'
+            example.lastname = 'Admin'
+            example.email = 'admin@example.com'
+            example.date_of_birth = "05/10/2000"
+            example.is_admin = True
+            example.content_filter = False
+            example.set_password('admin')
+            example.points = 1000
+            db.session.add(example)
+            db.session.commit()
+        except IntegrityError:
+            pass
+
 
 
 def main():
@@ -13,6 +34,7 @@ def main():
     app.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../users.db'
     db.init_app(app.app)
     db.create_all(app=app.app)
+    _create_admin(app)
     app.run(port=8080)
 
 
